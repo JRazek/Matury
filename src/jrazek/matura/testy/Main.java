49f5b03b537d6c9ece0ba8d350ac3ed8cc6331de
Jakub <jakub.jakubecky123@gmail.com>
2020-12-01 11:07:43 +0100

    los commitos

--- src/jrazek/matura/testy/Main.java
+++ src/jrazek/matura/testy/Main.java
@@ -1,15 +1,57 @@
 package jrazek.matura.testy;
 
+
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    static List<String> list = new ArrayList();
+private static class Pair<T,E>{
+    T k;
+    E v;
+}
+    static int binToDec(String bin){
+        int dec = 0;
+        int pow = 0;
+        for(int i = bin.length() - 1; i >= 0; i --){
+            int cv = Character.getNumericValue(bin.charAt(i));
+            dec += cv * Math.pow(2, pow);
+            pow ++;
+        }
+        return dec;
+    }
+    static String decToBin(int dec){
+        StringBuilder reversedNum = new StringBuilder();
+        while(dec != 0){
+            reversedNum.append(dec % 2);
+            dec /= 2;
+        }
+        StringBuilder ok = new StringBuilder();
+        for(int i = reversedNum.length() - 1; i >= 0; i --){
+            ok.append(reversedNum.charAt(i));
+        }
+        return ok.toString();
+    }
+    static String decToHex(int dec){
+        String table = "0123456789ABCDEF";
+        StringBuilder reversedTable = new StringBuilder();
+        while(dec != 0){
+            reversedTable.append(table.charAt(dec % 16));
+            dec /= 16;
+        }
+        reversedTable.reverse();
+        return reversedTable.toString();
+    }
+    public static Pair<Integer, Integer> findPair(int num){
+        List< Pair< Integer, Integer > > applicable;
+        for(int i = 0; i <= num/2; i ++){
+            int n1 = i;
+            int n2 = num - i;
+            float rate = Math.round( 10.f*((float)n1)/((float)n2) ) / 10.f;
 
+        }
+        return new Pair<>();
+    }
     public static void main(String[] args) {
-        list = FileReader.returnLines(new File("./DANE_PR/dziennik.txt"));
-        Zad.doYourJob1();
-        Zad.doYourJob2(4);
+
     }
 }
--- src/jrazek/matura/testy/Zad.java
+++ src/jrazek/matura/testy/Zad.java
@@ -1,62 +1,28 @@
 package jrazek.matura.testy;
 
+import java.util.List;
+
 public class Zad {
-    public static void doYourJob1(){
-        for(int i = -4; i <= 14; i += 3){
-            System.out.print(i);
-            System.out.print(", ");
-        }
-        System.out.println();
-        for(int i = -4; i <= 14; i ++){
-            if(i == 2) break;
-            System.out.print(i);
-            System.out.print(", ");
-        }
-        System.out.println();
-        for(int i = 12; i >= -12; i -=4){
-            System.out.print(i);
-            System.out.print(", ");
-        }
-        for(int i = 12; i >= -12; i -=4){
-            if(i == 0) break;
-            System.out.print(i);
-            System.out.print(", ");
-        }
-        System.out.println();
-    }
-    public static void doYourJob2(int n){
-        System.out.println();
-        System.out.println(4f/(n+1));
-        float sum1 = 0;
-        float sum2 = 0;
-        float product1 = 1;
-        for(int i = 1; i < n; i ++){
-            sum1+=(1f/(2f*i));
-        }
-        System.out.println(sum1);
+    public static List<Integer> bubbleSort(List<Integer> l){
+        if(l.size() > 1){
+            boolean done = false;
+            while(!done){
+                int buffer;
+                done = true;
+                for(int i = 0; i < l.size() - 1; i ++){
+                    if(l.get(i) < l .get(i + 1)){
+                        buffer = l.get(i);
+                        l.set(i, l.get(i + 1));
+                        l.set(i + 1, buffer);
+                        done = false;
+                    }
 
-        for(int i = 1; i <= n; i ++){
-            product1*=((float)(i+3)/n);
+                }
+            }
         }
-        System.out.println(product1);
-
-        for(int i = 1; i <= n; i ++){
-            sum2 += ((2f*(3*Math.pow(2,i-1)))/i);
-        }
-        System.out.println(sum2);
+        return l;
     }
     public static void doYourJob3(int n){
-        //a
-        float sum1 = 0;
-        float product1 = 1;
-        for(int i = 1; i <= n; i++){
-            sum1+=(2+3*(i-1))*(Math.pow(-1,i));
-        }
-        for(int i = 1; i <= n; i++){
-            product1 *= (4*i)*Math.pow(-1,i);
-        }
-        System.out.println();
-        System.out.println(sum1);
-        System.out.println(product1);
+
     }
 }
